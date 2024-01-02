fd50fba6a933578925145cb6f609c516dceff9b3
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 19:06:41 -0300

    feat:creating Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,20 @@
+import Social from "./Social";
 
 const Profile = () => {
-  const myAge = new Date().getFullYear() - 2004
+  const myAge = new Date().getFullYear() - 2004;
   return (
-    <div className="flex justify-center items-center flex-col p-3">
-    <img src="https://avatars.githubusercontent.com/u/103083001?v=4" alt="my github profile" className="rounded-full w-2/4" />
-    <h1>Lucas - {myAge}y</h1>
+    <div className='flex justify-center items-center flex-col p-3'>
+      <img
+        src='https://avatars.githubusercontent.com/u/103083001?v=4'
+        alt='my github profile'
+        className='rounded-full w-2/4 mb-2'
+      />
+      <h1>Lucas - {myAge}y</h1>
+      <div className="flex">
+        <Social link={"https://www.github.com/lucwx"} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
