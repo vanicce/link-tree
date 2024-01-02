2370a0927d3b66bc9089a3bc568c02083fcca16f
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 19:38:36 -0300

    feat: improving social component

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,11 +1,15 @@
-const Social = ({link}) => {
+const Social = ({ link }) => {
   return (
-    <div className="rounded-md ring-1 ring-black p-2">
-      <a href={link} target='_blank' rel='noreferrer' className="flex gap-2 items-start">
-        <img src='https://www.svgrepo.com/show/512317/github-142.svg' alt='' className="w-5"/>
+    <a href={link} target='_blank' rel='noreferrer'>
+      <div className='rounded-md ring-1 ring-black p-2 flex gap-2 items-start'>
+        <img
+          src='https://www.svgrepo.com/show/512317/github-142.svg'
+          alt=''
+          className='w-5'
+        />
         GitHub
-      </a>
-    </div>
+      </div>
+    </a>
   );
 };
 
