6bd8988341b356a6a1866c9f2d064e8a53e84892
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 19:05:49 -0300

    feat: creating pages component

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -1,10 +1,12 @@
-
 const Pages = () => {
   return (
-    <>
-    <h1> teste </h1>
-    </>
-  )
-}
+    <a href='https://open.spotify.com/playlist/6ddhyqtVZj1VJ9dUU4dp1c?si=c9550d169d85466a' target="_blank" rel="noreferrer">
+      <div className='rounded-md ring ring-black p-3 m-4 flex items-center gap-2'>
+        <img src="https://www.svgrepo.com/show/512899/spotify-162.svg" alt=""  className="w-8"/>
+        My Spotify Playlist
+      </div>
+    </a>
+  );
+};
 
-export default Pages
+export default Pages;
