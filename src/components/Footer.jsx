e28fabf9dbecfc7fb06f6c7608b7a0c30268461b
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 19:37:09 -0300

    feat: improving footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div className='fixed bottom-0 flex justify-center w-screen'>
-      {year} &copy;
+    <div className='fixed bottom-0 text-center border-t-2 p-2'>
+      Made With <a href="https://www.react.dev" className="underline"> React</a> and <a href="https://www.tailwindcss.com" className="underline">TailwindCSS</a> Lucas - {year}
     </div>
   );
 };
