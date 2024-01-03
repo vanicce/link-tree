2d639493e133fd2e5f57270657f4742e8bf0ac7f
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 21:00:13 -0300

    fix: fixing bad layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div className='fixed bottom-0 text-center border-t-2 p-2'>
+    <div className='text-center border-2 p-2'>
       Made With <a href="https://www.react.dev" className="underline"> React</a> and <a href="https://www.tailwindcss.com" className="underline">TailwindCSS</a> Lucas - {year}
     </div>
   );
