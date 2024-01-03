40427bd82c72eead0f12cedcdba6002cb5a8272c
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 21:02:10 -0300

    feat: improving responsive design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Profile from "./components/Profile";
-import Pages from "./components/Pages";
+import Page from "./components/Page";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="bg-neutral-100 h-[100dvh] selection:bg-neutral-800 selection:text-neutral-50">
-      <Profile/>
-      <Pages />
+    <div className='bg-neutral-100 h-[100dvh] selection:bg-neutral-800 selection:text-neutral-50 flex justify-center'>
+      <div className="w-full max-w-lg">
+      <Profile />
+      <p className='text-center'>you can see something useful here: </p>
+      <Page />
+      <Page />
+      <Page />
+      <Page />
+      <Page />
+      <Page />
+      <Page />
       <Footer />
+      </div>
     </div>
   );
 }
