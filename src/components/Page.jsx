4e64553c01fb525f836448f13764317137826dff
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 22:13:55 -0300

    feat: using props in page component

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,15 +1,12 @@
-const Pages = () => {
-  const links = {
-    spot:'https://open.spotify.com/playlist/6ddhyqtVZj1VJ9dUU4dp1c?si=c9550d169d85466a'
-  }
+const Pages = (props) => {
   return (
     <>
-    <a href={links.spot} target="_blank" rel="noreferrer">
-      <div className='rounded-md ring-1 ring-black p-3 m-4 flex items-center justify-center gap-2'>
-        <img src="https://www.svgrepo.com/show/512899/spotify-162.svg" alt=""  className="w-8"/>
-        My Spotify Playlist
-      </div>
-    </a>
+      <a href={props.link} target='_blank' rel='noreferrer'>
+        <div className='rounded-md ring-1 ring-black p-3 m-4 flex items-center justify-center gap-2 whitespace-nowrap'>
+          <img src={props.icon} alt='' className='w-7' />
+          <p>{props.title}</p>
+        </div>
+      </a>
     </>
   );
 };
