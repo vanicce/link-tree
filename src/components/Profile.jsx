1e5ce91361cd6dd057b8981f0a6b13c42e4e265f
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 22:42:16 -0300

    feat: improving date validation

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,9 @@
 import Social from "./Social";
 
 const Profile = () => {
-  const myAge = new Date().getFullYear() - 2004;
+  const myAge = Math.floor(
+    (new Date() - new Date("2004-06-01")) / (365 * 24 * 60 * 60 * 1000)
+  );
   return (
     <div className='flex justify-center items-center flex-col p-3 '>
       <img
@@ -10,11 +12,22 @@ const Profile = () => {
         className='rounded-full ring ring-[#4751F1] ring-offset-2 w-2/4 mb-2'
       />
       <h1>Lucas - {myAge}y</h1>
-      <p className="text-center">a little lost but always in search of knowledge.</p>
-      <div className="flex m-2 gap-2">
-        <Social link={"https://www.github.com/lucwx"} icon={"https://www.svgrepo.com/show/512317/github-142.svg"} />
-        <Social link="https://www.linkedin.com/in/pedro-lucas-santos-silva" icon={"https://www.svgrepo.com/show/521725/linkedin.svg"}/>
-        <Social link="https://www.x.com/lucassun_" icon={"https://www.svgrepo.com/show/513008/twitter-154.svg"}/>
+      <p className='text-center'>
+        a little lost but always in search of knowledge.
+      </p>
+      <div className='flex m-2 gap-2'>
+        <Social
+          link={"https://www.github.com/lucwx"}
+          icon={"https://www.svgrepo.com/show/512317/github-142.svg"}
+        />
+        <Social
+          link='https://www.linkedin.com/in/pedro-lucas-santos-silva'
+          icon={"https://www.svgrepo.com/show/521725/linkedin.svg"}
+        />
+        <Social
+          link='https://www.x.com/lucassun_'
+          icon={"https://www.svgrepo.com/show/513008/twitter-154.svg"}
+        />
       </div>
     </div>
   );
