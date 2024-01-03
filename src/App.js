4b313a124cfca1a0bd1a90c744ad3b8a2bc6914d
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 22:13:32 -0300

    feat: using loop to display sites

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import Profile from "./components/Profile";
 import Page from "./components/Page";
-import Footer from "./components/Footer";
 
 function App() {
+  const sites = [
+    {
+      title: "my spotify playlist",
+      link: "https://open.spotify.com/playlist/6ddhyqtVZj1VJ9dUU4dp1c?si=c9550d169d85466a",
+      icon: "https://www.svgrepo.com/show/512899/spotify-162.svg",
+    },
+    {
+      title: "linux terminal cheatsheet",
+      link: "https://linuxcommandlibrary.com/",
+      icon: "https://www.svgrepo.com/show/503411/linux.svg"
+    },
+    {
+      title: "my portfolio",
+      link: "https://lucas-portfolio-navy.vercel.app",
+      icon: "https://www.svgrepo.com/show/433647/pc1-f.svg"
+    }
+  ];
+  const sitePages = sites.map((site) => (
+    <Page title={site.title} link={site.link} icon={site.icon} />
+  ));
   return (
-    <div className='bg-neutral-100 h-[100dvh] selection:bg-neutral-800 selection:text-neutral-50 flex justify-center'>
-      <div className="w-full max-w-lg">
-      <Profile />
-      <p className='text-center'>you can see something useful here: </p>
-      <Page />
-      <Page />
-      <Page />
-      <Page />
-      <Page />
-      <Page />
-      <Page />
-      <Footer />
+    <div className='bg-neutral-100 selection:bg-neutral-800 selection:text-neutral-50 flex justify-center '>
+      <div className='w-full max-w-lg h-full min-h-[100dvh]'>
+        <Profile />
+        <p className='text-center'>you can see something useful here: </p>
+        {sitePages}
       </div>
     </div>
   );
