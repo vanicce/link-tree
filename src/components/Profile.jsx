276ee2df7b4225eedecb97d4acda4a3d391974d3
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 21:01:25 -0300

    feat: adding social components to profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,12 +7,14 @@ const Profile = () => {
       <img
         src='https://avatars.githubusercontent.com/u/103083001?v=4'
         alt='my github profile'
-        className='rounded-full w-2/4 mb-2'
+        className='rounded-full ring ring-[#4751F1] ring-offset-2 w-2/4 mb-2'
       />
       <h1>Lucas - {myAge}y</h1>
       <p className="text-center">a little lost but always in search of knowledge.</p>
-      <div className="flex m-2">
-        <Social link={"https://www.github.com/lucwx"} />
+      <div className="flex m-2 gap-2">
+        <Social link={"https://www.github.com/lucwx"} icon={"https://www.svgrepo.com/show/512317/github-142.svg"} />
+        <Social link="https://www.linkedin.com/in/pedro-lucas-santos-silva" icon={"https://www.svgrepo.com/show/521725/linkedin.svg"}/>
+        <Social link="https://www.x.com/lucassun_" icon={"https://www.svgrepo.com/show/513008/twitter-154.svg"}/>
       </div>
     </div>
   );
